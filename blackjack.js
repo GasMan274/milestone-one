dc9f43b180c89c06981af5861dc922e52a4e2b05
lawrence graham <lawrencegraham@lawrences-iMac.local>
2022-07-26 10:00:53 -0400

    added getvalue function

--- blackjack.js
+++ blackjack.js
@@ -1,23 +1,25 @@
-const dealSum = 0;
+ const dealSum = 0;
 const yourSum = 0;
 
 const dealerAceCount = 0;
 const yourAceCount = 0;
 
-var hidden;
-var deck;
+const hidden;
+const deck;
 
 const canHit = true; 
 //allows the player (you) to draw while yourSum <=21
 
 window.onload = function() {
-    buildDeck
-    shuffleDeck
+    buildDeck()
+    shuffleDeck()
+    startGame()
+
 }
 
 function buildDeck() {
-    let values = ["A", "2", "3", "4","5", "6", "7", "8", "9", "10", "J", "Q,", "K",];
-    let types = ["C", "D", "H", "S",];
+    let values = ["A ", "2 ", "3 ", "4 ","5 ", "6 ", "7 ", "8 ", "9 ", "10 ", "J ", "Q,", "K ",];
+    let types = ["C ", "D ", "H ", "S ",];
     deck = [];
  // this dbl for loop is crealted to 0cover all the card eg. 
 // A-C -> K-C -> A-D -> K-D etc.
@@ -35,4 +37,27 @@ function shuffleDeck() {
         deck[j] = temp;
     }
     console.log(deck);
+}
+
+function startGame() {
+    hidden = deck.pop();
+    dealSum += getvalue(hidden);
+    dealerAceCount += checkAce(hidden);
+    //console.log(hidden);
+    //console.log(dealerSum);
+    while (dealSum > 17) {
+
+    }
+}
+
+function getvalue(card) {
+    let data = card.split("-"); // 4 is the value and C is the type
+    let value = data[0]; 
+
+    if (isNaN(value)) {
+        if (value == "A"){
+            return 11;
+        }
+        return 10;
+    }
 }
