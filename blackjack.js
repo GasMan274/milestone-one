673edbe7aadf00f115056a980d205e629fdbea7b
lawrence graham <lawrencegraham@lawrences-iMac.local>
2022-07-26 21:40:37 -0400

    added code to .js & html

--- blackjack.js
+++ blackjack.js
@@ -1,39 +1,40 @@
- const dealSum = 0;
-const yourSum = 0;
+var dealerSum = 0;
+var yourSum = 0;
 
-const dealerAceCount = 0;
-const yourAceCount = 0;
+var dealerAceCount = 0;
+var yourAceCount = 0;
 
-const hidden;
-const deck;
+ var hidden;
+ var deck;
 
-const canHit = true; 
+var canHit = true; 
 //allows the player (you) to draw while yourSum <=21
 
 window.onload = function() {
-    buildDeck()
-    shuffleDeck()
-    startGame()
-
+    buildDeck();
+    shuffleDeck();
+    startGame();
+//console.log("newGame")
 }
 
 function buildDeck() {
-    let values = ["A ", "2 ", "3 ", "4 ","5 ", "6 ", "7 ", "8 ", "9 ", "10 ", "J ", "Q,", "K ",];
-    let types = ["C ", "D ", "H ", "S ",];
+    let values = ["A ", "2 ", "3 ", "4 ","5 ", "6 ", "7 ", "8 ", "9 ", "10 ", "J ", "Q,", "K "];
+    let types = ["C ", "D ", "H ", "S "];
     deck = [];
- // this dbl for loop is crealted to 0cover all the card eg. 
+ // this dbl for loop is created to cover all the card etc. 
 // A-C -> K-C -> A-D -> K-D etc.
-for (let i = 0; i < types.length; i++) {
-    for (let j = 0; j < values.length; j++); {
-        deck.push(values[j]+ "-" + types[i]); 
+    for (let i = 0; i < types.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+        deck.push(values[j] +  "-"  + types[i]); 
     }
 }
-//console.log(deck);
+    //console.log(deck);
 }
 function shuffleDeck() {
     for (let i  = 0; i < deck.length; i++){
-        let j = Math.floor(Math.random() * deck.length);// (0-1) * 52 => (0-51.9999)
+        let j = Math.floor(Math.random() * deck.length); // (0-1) * 52 => (0-51.9999)
         let temp = deck[i];
+        deck[i] = deck[j];
         deck[j] = temp;
     }
     console.log(deck);
@@ -41,23 +42,55 @@ function shuffleDeck() {
 
 function startGame() {
     hidden = deck.pop();
-    dealSum += getvalue(hidden);
+    dealerSum += getvalue(hidden);
     dealerAceCount += checkAce(hidden);
     //console.log(hidden);
     //console.log(dealerSum);
-    while (dealSum > 17) {
+    while (dealerSum > 17) {
+        //<img src="./cards/4-c.png">
+        let cardImg = document.createElement("img")
+        let card = deck.pop(); 
+        cardImg.src = "./cards/" + card + "png";
+        dealerSum += getvalue(card);
+        dealerAceCount += checkAce(card);
+        document.getElementById("dealer-cards").append(cardImg)
 
+    }
+    console.log(dealerSum);
+    for (let i = 0; i < 2; i++) {
+        let cardImg = document.createElement("img")
+        let card = deck.pop(); 
+        cardImg.src = "./cards/" + card + "png";
+        yourSum += getvalue(card);
+        yourAceCount += checkAce(card);
+        document.getElementById("your-cards").append(cardImg)
+    }
+    console.log(yourSum);
+    document.getElementById("hit").addEventListener("click", hit);
+}
+function hit(){
+    if (!canHit) {
+        return;
     }
 }
 
 function getvalue(card) {
-    let data = card.split("-"); // 4 is the value and C is the type
+    let data = card.split("-"); // 4 is the "value" and C is the "type" also the "-" is also 0for the discription 
     let value = data[0]; 
 
-    if (isNaN(value)) {
+    if (isNaN(value)) { // cause the value could not be a number it can be a string "A, J, Q, K," Ace could be 11
         if (value == "A"){
             return 11;
         }
         return 10;
     }
+    return parseInt(value);
 }
+
+function checkAce(card) {
+    if (card[0] == "A") {
+        return 1;
+    }
+    return 0;
+}
+    
